4210b704bb4097aa09b0f51902789b4b4cc16618
Younes Rabeh <rabehyounes9@gmail.com>
2023-08-12 15:00:46 +0100

    adding_feature

--- App/Layout/files_canvas_covers.py
+++ App/Layout/files_canvas_covers.py
@@ -7,11 +7,12 @@ from PIL import Image, ImageTk
 
 class FilesImages:
     CANVAS = None
-    _width, _height, NUM = 0, 0, 1
+    _width, _height, NUM = 0, 0, 0
     project_name, projects_dir, project_directory = "", "", ""
     resized_image, image = "", ""
     FILE_LIST = []  # file names
     min_canvas_width, min_canvas_height = "", ""
+
 
     def __init__(self, main_canvas, _width, _height, _dir, project_name, wanted_folder):
         self.CANVAS = main_canvas
@@ -28,6 +29,8 @@ class FilesImages:
         self.update_canvas(wanted_folder)
 
     def update_canvas(self, wanted_folder):
+        self.NUM = 1
+
         files = GetInProject(self.project_name, self.project_directory)
         if wanted_folder == "Videos":
             self.FILE_LIST = files.get_videos()
@@ -42,6 +45,7 @@ class FilesImages:
         print(f"\033[{36}m{info}\033[0m")  # Set the console color to Cyan
         image_list = []  # Temporary list to store the images (actual img)
         i, j, k = 0, 0, 0
+        height_adding, row_counter = 0, 0
 
         for img_name in self.FILE_LIST:
             self.image = Image.open(self.project_directory + f"/{wanted_folder}/" + img_name)
@@ -50,15 +54,22 @@ class FilesImages:
             image_list.append(self.image)  # Add the image to the temporary list
             self.CANVAS.create_image(120 + j, 70 + i, image=self.image)
             if k == 3:
-                i = i + 140  # Y-axis
-                j = 0        # X-axis
-                k = 0        # Row
-                self.NUM += 1
+                i = i + 140            # Y-axis
+                j = 0                  # X-axis
+                k = 0                  # Column
+                row_counter += 1  # Row
             else:
                 j += 240
                 k += 1
+
         # TODO: not update --canvas (so big)
+        print(self.NUM)
+        if row_counter > 3:
+            for i in range(row_counter - 3):
+                self.NUM += 1
+        for i in range(self.NUM):
+            height_adding += 5
 
         # Assign the temporary image list to self.FILE_LIST after the loop has finished
         self.FILE_LIST = image_list
-        self.CANVAS.configure(scrollregion=(0, 0, self._width,  (self._height // 3 + 50) * self.NUM))
+        self.CANVAS.configure(scrollregion=(0, 0, self._width, ((self._height * self.NUM) + 290 + height_adding)))
